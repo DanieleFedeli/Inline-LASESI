efa64e8fd8a7860b265fe6973d352397ecb21b0c
DanieleFedeli <danieligno10@gmail.com>
2018-07-14 02:36:23 +0200

    Fixed migration for deploy

--- db/migrate/20180525080352_create_users.rb
+++ db/migrate/20180525080352_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password
-      t.json :room_host
-      t.boolean :admin
+      t.string  :username
+      t.string  :email
+      t.string  :password
+      t.json    :room_host
+      t.boolean :admin, default: false
       t.json :rooms
       t.json :facebook
       t.json :google
