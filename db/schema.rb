b4350fda5e291906e3ac0cf30ac14ba919c4a14f
DanieleFedeli <danieligno10@gmail.com>
2018-07-14 13:43:12 +0200

    Added admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_234121) do
+ActiveRecord::Schema.define(version: 2018_07_14_114217) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "room_id"
